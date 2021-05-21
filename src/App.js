626333bbada6dc28b871cc264319ed1f643539c3
Sara Alkhamri <s.alkhamri@gmail.com>
2021-05-20 17:57:38 -0700

    Created all pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter className="App">
       <h1>Hi Sara</h1>
-    </div>
+      <Nav />
+      <Hero />
+      <Route path="/about" component={About} />
+      <Services />
+      <Contact />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>I am Hero</h1>
+    </div>
+  );
+}
